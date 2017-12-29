40b777fb655b7e23b6922931b6b713fcc0f57036
Raymond Cohen <raycohen@gmail.com>
2017-12-29 13:41:42 -0500

    Add a basic working jest setup

--- src/index.js
+++ src/index.js
@@ -3,15 +3,10 @@
  */
 
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 import './styles.css'
 
 export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
   render() {
     const {
       text
